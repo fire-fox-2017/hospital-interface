6ef64337d34a45639d62cbfd4e9acdc8934e87b1
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-04-04 19:33:47 +0800

    readline get username and password

--- hospital.js
+++ hospital.js
@@ -5,6 +5,27 @@ class Hospital {
     this.patients = patients
     this.location = location
   }
+
+  addPatients(name, diagnosis) {
+
+  }
+
+  isUsernameExist(username) {
+    for (let i = 0 ; i < this.employees.length ; i++) {
+      if (this.employees[i].username == username)
+        return true;
+    }
+    return false;
+  }
+
+  checkPassword(username, password) {
+    for (let i = 0 ; i < this.employees.length ; i++) {
+      if (this.employees[i].username == username)
+        return (this.employees[i].password == password) ? true : false;
+    }
+    return false;
+  }
+
 }
 
 class Patient {
@@ -49,6 +70,8 @@ class OfficeBoy extends Employee {
   }
 }
 
+
+// Initialization ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // create employees
 let admin = new Admin("Rudy", "rudy", "haha" );
 let doctor = new Doctor("James", "james", "haha" );
@@ -61,19 +84,75 @@ employees.push(doctor);
 employees.push(receptionist);
 employees.push(ob);
 
-
 // create patients
-let patients = [];
 let p1 = new Patient(1, "Baba", "Hamstring");
-
+let patients = [];
 patients.push(p1);
 
+let hos_name = "HoHoHo Hospital";
+let hos_address = "Jalan Sultan 4040, Jakarta Selatan";
 // create hospital
-let hospital = new Hospital();
+let hospital = new Hospital(hos_name, hos_address, employees, patients);
+
+console.log(hospital);
+
+// Hospital Interface ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+let str = '';
+
+str = `Welcome to ${hospital.name}\n`;
+str += "--------------------------\n";
+console.log(str);
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.setPrompt("Please enter your username: ");
+rl.prompt();
+
+let is_username_valid = false;
+let is_password_valid = false;
+let ask_for_pass = false;
+
+let username = "";
+let password = "";
+rl.on('line', (input) => {
+  // find username
+
+  if (hospital.isUsernameExist(input) && !is_username_valid) {
+    username = input;
+    console.log("hore");
+    is_username_valid = true;
+    rl.setPrompt("Please enter your password:");
+    rl.prompt();
+
+  }
+  else if (!hospital.isUsernameExist(input) && !is_username_valid) {
+    console.log("Incorrect Username");
+    rl.setPrompt("Please enter your username:");
+    rl.prompt();
+  }
+
+  if(is_username_valid && !is_password_valid) {
+    if (hospital.checkPassword(username, input)) {
+      console.log("password correct");
+      is_password_valid = true;
+    }
+    else {
+      rl.setPrompt("Please enter your password:");
+      rl.prompt();
+    }
+  } else if (is_username_valid && is_password_valid) {
+    console.log("hahaha");
+  }
 
 
 
 
+});
 
 
 
