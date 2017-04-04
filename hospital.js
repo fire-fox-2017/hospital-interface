f2a274c948673f44e118d80b49f86daaff213564
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-04-04 15:29:03 +0800

    release 0: still figuring out the attributes and etc

--- hospital.js
+++ hospital.js
@@ -23,3 +23,61 @@ class Employee {
     this.password = password
   }
 }
+
+class Admin extends Employee {
+  constructor(name, username, password) {
+    super(name, "ADMIN", username, password);
+  }
+}
+
+class Doctor extends Employee {
+  constructor(name, username, password) {
+    super(name, 'DOCTOR', username, password);
+
+  }
+}
+
+class Receptionist extends Employee {
+  constructor(name, username, password) {
+    super(name, 'RECEPTIONIST', username, password);
+  }
+}
+
+class OfficeBoy extends Employee {
+  constructor(name, username, password) {
+    super(name, 'OFFICEBOY', username, password);
+  }
+}
+
+// create employees
+let admin = new Admin("Rudy", "rudy", "haha" );
+let doctor = new Doctor("James", "james", "haha" );
+let receptionist = new Receptionist("Sharla", "sharla", "haha" );
+let ob = new OfficeBoy("Toby", "toby", "haha");
+
+let employees = [];
+employees.push(admin);
+employees.push(doctor);
+employees.push(receptionist);
+employees.push(ob);
+
+
+// create patients
+let patients = [];
+let p1 = new Patient(1, "Baba", "Hamstring");
+
+patients.push(p1);
+
+// create hospital
+let hospital = new Hospital();
+
+
+
+
+
+
+
+
+
+
+//
