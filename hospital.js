f79cde04b9ac7a305a45c39583b969eb44f82303
Didit Aditya <diditaditya@users.noreply.github.com>
2017-04-08 16:11:41 +0700

    hospital interface is ready

--- hospital.js
+++ hospital.js
@@ -8,11 +8,12 @@ class Hospital {
 }
 
 class Patient {
-  constructor(id, name, diagnosis) {
+  constructor(id, name, records) {
     this.id = id;
     this.name = name;
-    this.diagnosis = diagnosis;
+    this.records = records;
   }
+  
 }
 
 class Employee {
@@ -25,11 +26,11 @@ class Employee {
   }
 
   setAccessLevel(position) {
-    if (position === "admin") {
+    if (position.toLowerCase() === "admin") {
       return "admin";
-    } else if (position === "doctor") {
+    } else if (position.toLowerCase() === "doctor") {
       return "doctor";
-    } else if (position === "employee"){
+    } else if (position.toLowerCase() === "employee"){
       return "employee";
     } else {
       return "none";
@@ -47,10 +48,14 @@ class Process {
     this.user = "";
     this.access_level = "";
     this.menuList = ["list_patients                                         to show the patients list",
+					 "add_patient <name>                                    to register new patient",
+					 "remove patient <patient_id>                           to remove patient from the data",
                      "view_records <patient_id>                             to show a patient's record",
-                     "add_record <patient_id>                               to add record to a patient",
+                     "add_record <patient_id> <keyword> <record_content>    to add record to a patient",
                      "remove_record <patient_id> <record_id>                to remove a record of a patient",
-                     "add_employee <name> <position> <username> <password>  to add an employee to the system",
+                     "list_accounts                                         to show the registered accounts",
+                     "add_account <name> <position> <username> <password>   to register new account to the system",
+                     "remove_account <username>                             to remove an account from the sytem",
                      "log_out                                               to stop using the terminal"];
     this.border = "--------------------------------------------------------------------------------------";
   }
@@ -105,11 +110,11 @@ class Process {
     } else if (this.access_level === "doctor") {
       console.log(question);
       console.log(optionTitle);
-      options = fullMenu - 1;
+      options = 6;
     } else if (this.access_level === "employee") {
       console.log(question);
       console.log(optionTitle);
-      options = fullMenu - 1;
+      options = 6;
     } else {
       console.log("Thank you for logging in, however you are not authorized to use this terminal.")
       console.log("Logging out..");
@@ -132,14 +137,72 @@ class Process {
 		this.logOut();
 	  } else if (command[0] === "view_records") {
 		this.viewRecord(Number(command[1]));
+	  } else if (command[0] === "add_patient") {
+		let name = [];
+		for (let i = 1; i < command.length; i++) {
+		  name.push(command[i]);
+	    }
+		this.addPatient(name.join(" "));
+	  } else if (command[0] === "remove_patient") {
+		this.removePatient(Number(command[1]));
+	  } else if (command[0] === "add_record") {
+		let content = [];
+		for (let i = 3; i < command.length; i++) {
+	      content.push(command[i]);
+		}
+		this.addRecord(Number(command[1]), command[2], content.join(" "));
+	  } else if (command[0] === "remove_record") {
+		this.removeRecord(Number(command[1]), Number(command[2]));
+	  } else if (command[0] === "add_account") {
+	    if (this.access_level === "admin" && command.length < 5) {
+		  console.log("Please re-check the required input for new account.");
+		} else {
+		  this.addAccount(command[1], command[2], command[3], command[4]);
+		} 
+	  } else if (command[0] === "remove_account") {
+	    this.removeAccount(command[1]);
+	  } else if (command[0] === "list_accounts") {
+		this.listAccounts();
+      } else {
+		console.log("Command is not found./n");
+		this.menu();
 	  }
     });
   }
   
   listPatients() {
-	console.log(this.patients);
+	console.log("\nID  Patient Name");
+	for (let i = 0; i < this.patients.length; i++) {
+	  console.log(`${this.patients[i].id}   ${this.patients[i].name}`);
+	}
 	console.log("\n");
 	this.menu();
+  }
+  
+  addPatient(name) {
+    if (name.length > 0) {
+      let id = this.assignPatientID();
+	  let patient = new Patient(id, name, []);
+	  this.patients.push(patient);
+	  console.log(`The new patient ${name} with id = ${id} has been successfully added.\n`); 
+	  this.listPatients();
+	} else {
+	  console.log("Please insert the patient name after the command.");
+	  this.menu();
+	}	
+  }
+  
+  removePatient(patientID) {
+    let index = this.findPatientIndex(patientID);
+    if(index !== null) {
+	  console.log(`Patient ${this.patients[index].name} with id = ${patientID} has been successfully removed.`);
+	  this.patients.splice(index,1);
+	  this.listPatients();
+	} else {
+	  console.log(`Patient ID = ${patientID} cannot be found in the database.`);
+	  console.log("\n");
+	  this.menu();
+	}
   }
   
   viewRecord(patientID) {
@@ -149,10 +212,90 @@ class Process {
 	  console.log("\n");
 	  this.menu();
 	} else {
-	  console.log(`Patient ID = ${patientID} cannot be found in the database.`);
+	  console.log(`Patient ID cannot be found in the database.`);
 	  console.log("\n");
 	  this.menu();
 	}
+  }
+  
+  addRecord(patientID, keyword, content) {
+	let patientIndex = this.findPatientIndex(patientID);
+	if (patientIndex !== null) {
+	  let id = this.assignRecordID(patientID);
+	  let record = {};
+	  record["id"] = id;
+	  record[keyword] = content;
+	  this.patients[patientIndex].records.push(record);
+	  console.log(`The records of patient ${this.patients[patientIndex].name} with id = ${patientID} has been updated.\n`);
+      this.viewRecord(patientID); 
+	} else {
+	  console.log(`Patient ID cannot be found in the database.`);
+	  console.log("\n");
+	  this.menu();
+	}
+  }
+  
+  removeRecord(patientID, recordID) {
+	let patientIndex = this.findPatientIndex(patientID);
+	let recordIndex = this.findRecordIndex(patientID, recordID);
+	if (patientIndex === null) {
+	  console.log(`Patient ID cannot be found in the database.`);
+	  console.log("\n");
+	  this.menu();
+	} else if(recordIndex === null) {
+	  console.log(`Record ID = ${recordID} cannot be found in patient with ID = ${patientID}.`);
+	  console.log("\n");
+	  this.menu();
+	} else {
+	  console.log("Record has been removed.");
+	  this.patients[patientIndex].records.splice(recordIndex, 1);
+	  this.viewRecord(patientID);
+	}
+  }
+  
+  addAccount(name, position, username, password) {
+	if (this.access_level === "admin") {
+	  if (position.toLowerCase() === "admin" || position === "doctor" || position === "employee" || position === "office boy") {
+	    let employee = new Employee(name, position, username, password);
+        this.employees.push(employee);
+        console.log("New account has been successfully created.\n");
+        this.menu();
+	  } else {
+		console.log("the entered 'position' is not recognized.\n");
+		this.menu();
+	  }
+	} else {
+	  console.log("Command is not found.\n");
+	  this.menu();
+	}
+  }
+  
+  removeAccount(username) {
+    if (this.access_level === "admin") {
+      let employeeIndex = this.findEmployeeIndex(username);
+      if (employeeIndex !== null) {
+		this.employees.splice(employeeIndex, 1);
+	    console.log("New account has been successfully created.\n");
+	    this.menu();
+	  } else {
+		console.log("Username is not found in the system.");
+		this.menu();
+	  }     
+	} else {
+	  console.log("Command is not found.\n");
+	  this.menu();
+	}  
+  }
+  
+  listAccounts() {
+     if (this.access_level === "admin") {
+	   console.log(this.employees);
+	   console.log("\n");
+	   this.menu();
+	 } else {
+	   console.log("Command is not found.\n");
+	   this.menu();
+	} 
   }
   
   logOut() {
@@ -194,6 +337,43 @@ class Process {
 	return index;
   }
   
+  assignPatientID() {
+	let id = 1;
+	while (this.findPatientIndex(id) !== null) {
+	  id++;
+    }
+    return id;
+  }
+  
+  findRecordIndex(patientID, recordID) {
+	let patientIndex = this.findPatientIndex(patientID);
+	let index = null;
+	for (let i = 0; i < this.patients[patientIndex].records.length; i++) {
+      if (this.patients[patientIndex].records[i].id === recordID) {
+		index = i;
+	  }
+	}
+	return index;
+  }
+  
+  assignRecordID(patientID) {
+	let id = 1;
+	while (this.findRecordIndex(patientID, id) !== null) {
+	  id++;
+    }
+    return id;
+  }
+  
+  findEmployeeIndex(username) {
+    let index = null;
+	for (let i = 0; i < this.employees.length; i++) {
+      if (this.employees[i].username === username) {
+		index = i;
+	  }
+	}
+	return index;
+  }
+  
   
 
 
@@ -216,13 +396,15 @@ employees.push(anto);
 
 // Creates patients
 let patients = [];
-let ucok = new Patient(1, "Ucok", "Malaria");
+let ucok = new Patient(1, "Ucok", [{id: 1, diagnosis: "Malaria"}]);
+let joko = new Patient(2, "Joko", [{id: 1, diagnosis: "Kutu air"}]);
 patients.push(ucok);
+patients.push(joko);
 
 // Creates hospital
 let hospital = new Hospital("mystic", "somewhere", employees, patients);
 
-//Initializes the interface
+// Initializes the interface
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
